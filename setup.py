73bc4b05fe280aa0669945eb44596832645a6d6e
EricJamesCrow <EricCrow@pm.me>
2022-02-09 19:26:22 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup
 
-REQUIREMENTS = ["cython",
-                "PyQt6~=6.2.3",
-                "PySide6~=6.2.3",
-                "seaborn~=0.11.2",
-                "pandas~=1.4.0",
-                "matplotlib~=3.5.1",
+REQUIREMENTS = ["PyQt6",
+		"PySide6",
+		"seaborn",
+		"pandas",
+		"matplotlib"
                 ]
 
 setup(
     name='E-PhysAnalyzer',
-    version='0.1.0',
+    version='0.1.7',
     packages=['E-PhysAnalyzer'],
-    url='',
+    url='https://github.com/EricJamesCrow/E-PhysAnalyzer',
     license='MIT',
     author='Cameron Cordero / Eric Crow',
-    author_email='cameroncordero@gmail.com / EricCrow@pm.me',
+    author_email='EricCrow@pm.me',
     install_requires=REQUIREMENTS
 )
