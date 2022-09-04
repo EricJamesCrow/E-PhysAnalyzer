68e3da3fc0cf10e8195ca7ead44b2bfda1b8a2a8
EricJamesCrow <EricCrow@pm.me>
2022-09-03 17:35:08 -0700

    Update app.js

--- E-PhysAnalyzer2/qml/javascript/app.js
+++ E-PhysAnalyzer2/qml/javascript/app.js
@@ -2,6 +2,8 @@ function determineScaleFactor(platform, screen){
     if(platform === "windows") {
         if(screen === 1.5) {
             return 1
+        } else {
+            return 1
         }
     } else if(platform === "linux") {
         if(screen === 1) {
